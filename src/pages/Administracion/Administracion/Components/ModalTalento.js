0216749775f5343dc741544409e496d8d6a5f02e
Jefferson Cargua <jeffersoncargua@gmail.com>
2024-12-17 19:23:01 -0500

    Proyecto Capernova Front-End correcion del modal de Talento

--- src/pages/Administracion/Administracion/Components/ModalTalento.js
+++ src/pages/Administracion/Administracion/Components/ModalTalento.js
@@ -68,9 +68,9 @@ export const ModalTalento = ({showModalTalento,setShowModalTalento,talento,setTa
         <div id="crud-modal" tabIndex='-1' className={`${showModalTalento? '' :'hidden'} overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] h-full bg-gray-700/[0.6]`}>
             <div className="relative p-4 mx-auto w-full max-w-2xl max-h-full">
                 {/*<!-- Modal content -->*/}
-                <div className="relative bg-white my-[10%] rounded-lg shadow dark:bg-gray-700 mb-14">
+                <div className="relative bg-white my-[4%] rounded-lg shadow dark:bg-gray-700">
                     {/*<!-- Modal header -->*/}
-                    <div className="flex items-center justify-between p-4 md:p-5 dark:border-gray-600">
+                    <div className="flex items-center justify-between p-4 md:p-5">
                         <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
                             {talento.Codigo ? 'Editar trabajador':'Agregar Nuevo trabajador'}
                         </h3>
